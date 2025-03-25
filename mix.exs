63cf79feab986ef7903f42ea9b99c1909e465ed5
Elton Searcy <esaucy@Eltons-MacBook-Pro.local>
2025-03-24 21:46:51 -0600

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Minne.MixProject do
     [
       app: :minne,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,10 +21,10 @@ defmodule Minne.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.10"},
-      {:ex_aws, "~> 2.1", optional: true},
-      {:ex_aws_s3, "~> 2.0", optional: true},
-      {:hackney, "~> 1.16", optional: true},
+      {:plug, "~> 1.17"},
+      {:ex_aws, "~> 2.2", optional: true},
+      {:ex_aws_s3, "~> 2.5", optional: true},
+      {:hackney, "~> 1.23", optional: true},
       {:sweet_xml, "~> 0.6.6", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}

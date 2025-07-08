0d8d24065a61fcd10d5199778796605aa5041207
Elton Searcy <esaucy@Eltons-MacBook-Pro.local>
2025-07-08 14:03:49 -0600

    add comment

--- lib/minne.ex
+++ lib/minne.ex
@@ -119,6 +119,7 @@ defmodule Minne do
         {conn, limit, [{name, %{headers: headers, body: body}} | acc]}
 
       {:file, name, upload} ->
+        # these fields can/will be ignored and defaulted to public if not provided for any reason
         auth_meta = Map.get(conn.assigns, :auth_meta, %{})
         privacy = Map.get(conn.assigns, :privacy, :public)
 
